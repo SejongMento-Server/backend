ec3ebefd4f75f187980ca5298952f56b4f170569
ykh4663 <superho7@gmail.com>
2025-08-26 07:44:01 +0900

    init setting

---
+++ src/main/java/com/sejongmento/backend/SejongmentoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sejongmento.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongmentoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejongmentoBackendApplication.class, args);
+	}
+
+}
